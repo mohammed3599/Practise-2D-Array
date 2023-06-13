f793034f71eb502197e524b29b8687868293a60c
mohammed3599 <kataloni1999@gmail.com>
2023-06-13 12:18:49 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Practise_2D_Array
+{
+    internal class Program
+    {
+
+        static void calSum()
+        {
+            int[,] array = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int sum = 0;
+
+            for (int row = 0; row < array.GetLength(0); row++)
+            {
+                for (int col = 0; col < array.GetLength(1); col++)
+                {
+                    sum = sum + array[row, col];
+                }
+            }
+
+            Console.WriteLine("The sum of all elements in the array is: " + sum);
+        }
+
+            static void Main(string[] args)
+             {
+                calSum();
+            }
+    }
+}
